f99f0ecde1978b55968d88561c41b762e7f43fca
Jose Vega <jocvegar@gmail.com>
2023-04-06 10:21:07 -0600

    add validations

--- app/models/registration_form.rb
+++ app/models/registration_form.rb
@@ -1,2 +1,16 @@
 class RegistrationForm < ApplicationRecord
+  extend FriendlyId
+	friendly_id :slug_it, use: [:slugged]
+
+  validates_presence_of :first_name, :last_name, :email, :date_of_birth, :on => :update
+
+  private
+
+  def slug_it
+	  slug_candidates = []
+		3.times do
+			slug_candidates <<  "registration-" + SecureRandom.random_number(1000).to_s + SecureRandom.urlsafe_base64(7).parameterize.downcase
+		end
+	  slug_candidates
+	end
 end
