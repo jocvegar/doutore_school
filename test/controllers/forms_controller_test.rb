5768da5c1fae8f4658a4fd23c8a5643b2b1ad670
Jose Vega <jocvegar@gmail.com>
2023-04-06 10:47:47 -0600

    add controller tests:

--- test/controllers/forms_controller_test.rb
+++ test/controllers/forms_controller_test.rb
@@ -1,8 +1,20 @@
 require "test_helper"
 
 class FormsControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
+  def setup
+    @user = create(:user)
+  end
+
+  test "should not get index" do
+    get root_path
+    assert_response :redirect
+  end
+
   test "should get index" do
-    get forms_index_url
+    sign_in @user
+    get root_path
     assert_response :success
   end
 end
